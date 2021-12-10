00eea9daf2ce83e3fa1c9bfbc06af62feb50ca4e
System Administrator <udin_rajkarnikar2001@yahoo.com>
2021-12-09 18:52:09 -0600

    First Commit

---
+++ SwiftUIChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChatApp
+//
+//  Created by Udin Rajkarnikar on 12/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, worl")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIChatApp/SwiftUIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatAppApp.swift
+//  SwiftUIChatApp
+//
+//  Created by Udin Rajkarnikar on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
