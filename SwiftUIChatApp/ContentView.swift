8b93754b4b4cae955d536e21aa001a036bad8681
System Administrator <udin_rajkarnikar2001@yahoo.com>
2021-12-10 12:59:32 -0600

    Login and Account creation page

--- SwiftUIChatApp/ContentView.swift
+++ SwiftUIChatApp/ContentView.swift
@@ -8,9 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isLogin = false
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Hello, worl")
-            .padding()
+        NavigationView{
+            
+            VStack(spacing: 10){
+                ScrollView{
+                    Picker(selection: $isLogin) {
+                        
+                        Text("Login")
+                            .tag(true)
+                        
+                        Text("Create Account")
+                            .tag(false)
+                    
+                    } label: {
+                        
+                        Text("Any topic")
+                    
+                    }.pickerStyle(SegmentedPickerStyle())
+                        
+                    if !isLogin{
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                                .foregroundColor(.black)
+                        }
+                        
+                    }
+                    
+                    
+                    Group{
+                        
+                        TextField("Email", text: $email)
+                                .keyboardType(.emailAddress)
+                                .autocapitalization(.none)
+                        
+                        
+                        SecureField("Password", text: $password)
+                            
+                    }
+                    .padding(14)
+                    .background(Color.white)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 7)
+                            .stroke(Color.white, lineWidth: 0.5)
+                    )
+                    .padding(.top, 10)
+                    
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack{
+                            Spacer()
+                            Text(isLogin ? "Login" : "Create Account")
+                                .foregroundColor(Color.white)
+                                .padding()
+                            Spacer()
+                        }.background(Color.blue)
+                    }.cornerRadius(5)
+                        .padding(.top)
+                
+                    
+                }.navigationTitle(isLogin ? "Login" : "Account Creation")
+            }.padding()
+                .background(Color(.init(white: 0, alpha: 0.05)).ignoresSafeArea())
+            
+        }
+      
     }
 }
 
